94c353b82662d94798599dd16add2422c7f6af74
poula <bola.hanna55@gmail.com>
2019-07-03 03:12:49 +0200

    fix issue in sidekiq worker

--- app/workers/publish_post_worker.rb
+++ app/workers/publish_post_worker.rb
@@ -2,7 +2,7 @@ class PublishPostWorker
   include Sidekiq::Worker
 
   def perform(id)
-    post = Post.find_by(id)
+    post = Post.find_by(id: id)
     post.publish! if post.present? && post.authenticated?
   end
 end
