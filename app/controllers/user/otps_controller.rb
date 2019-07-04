d8a1ee5514a3eb3002551e40fded475dce3db4bb
poula <bola.hanna55@gmail.com>
2019-07-04 09:05:30 +0200

    change order of otp controller functions

--- app/controllers/user/otps_controller.rb
+++ app/controllers/user/otps_controller.rb
@@ -3,6 +3,15 @@ class User::OtpsController < User::ApplicationController
     @otp = load_otp
   end
 
+  def send_otp
+    otp = load_otp
+    otp.mobile = params[:mobile]
+    otp.send_otp!
+    head :ok
+  rescue Authentication::AuthyError => e
+    render json: { error: e.message }, status: :bad_request
+  end
+
   def verify
     otp = load_otp
     otp.token = params[:token]
@@ -13,14 +22,7 @@ class User::OtpsController < User::ApplicationController
     render json: { error: e.message }, status: :bad_request
   end
 
-  def send_otp
-    otp = load_otp
-    otp.mobile = params[:mobile]
-    otp.send_otp!
-    head :ok
-  rescue Authentication::AuthyError => e
-    render json: { error: e.message }, status: :bad_request
-  end
+
 
   private
 
