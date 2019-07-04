ad41f269903999de76dc273242f349c72f0b845d
poula <bola.hanna55@gmail.com>
2019-07-04 09:57:19 +0200

    configure permitted params to allow username when signup

--- app/controllers/user/application_controller.rb
+++ app/controllers/user/application_controller.rb
@@ -5,7 +5,7 @@ class User::ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+
   skip_before_action :verify_authenticity_token
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -27,11 +27,5 @@ class User::ApplicationController < ActionController::Base
       flash.alert = 'you are not authorized'
       redirect_to root_path
     end
-  end
-
-  def configure_permitted_parameters
-    added_attrs = %i[username email password password_confirmation remember_me]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
